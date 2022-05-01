67e1b3ac3e171a6dbcdf5d4727d164517bbe4424
NineSong <zhangyjsh@gmail.com>
2022-05-02 01:00:58 +0800

    Update UI: high DPI

--- Netch/Forms/GlobalBypassIPForm.Designer.cs
+++ Netch/Forms/GlobalBypassIPForm.Designer.cs
@@ -28,36 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GlobalBypassIPForm));
             this.IPGroupBox = new System.Windows.Forms.GroupBox();
+            this.IPListBox = new System.Windows.Forms.ListBox();
+            this.IPTextBox = new System.Windows.Forms.TextBox();
+            this.PrefixComboBox = new System.Windows.Forms.ComboBox();
             this.AddButton = new System.Windows.Forms.Button();
             this.DeleteButton = new System.Windows.Forms.Button();
-            this.PrefixComboBox = new System.Windows.Forms.ComboBox();
-            this.IPTextBox = new System.Windows.Forms.TextBox();
-            this.IPListBox = new System.Windows.Forms.ListBox();
             this.ControlButton = new System.Windows.Forms.Button();
             this.IPGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // IPGroupBox
             // 
+            this.IPGroupBox.Controls.Add(this.IPListBox);
+            this.IPGroupBox.Controls.Add(this.IPTextBox);
+            this.IPGroupBox.Controls.Add(this.PrefixComboBox);
             this.IPGroupBox.Controls.Add(this.AddButton);
             this.IPGroupBox.Controls.Add(this.DeleteButton);
-            this.IPGroupBox.Controls.Add(this.PrefixComboBox);
-            this.IPGroupBox.Controls.Add(this.IPTextBox);
-            this.IPGroupBox.Controls.Add(this.IPListBox);
             this.IPGroupBox.Location = new System.Drawing.Point(12, 12);
             this.IPGroupBox.Name = "IPGroupBox";
-            this.IPGroupBox.Size = new System.Drawing.Size(316, 295);
+            this.IPGroupBox.Size = new System.Drawing.Size(497, 469);
             this.IPGroupBox.TabIndex = 0;
             this.IPGroupBox.TabStop = false;
             this.IPGroupBox.Text = "IPs";
             // 
+            // IPListBox
+            // 
+            this.IPListBox.FormattingEnabled = true;
+            this.IPListBox.ItemHeight = 24;
+            this.IPListBox.Location = new System.Drawing.Point(6, 30);
+            this.IPListBox.Name = "IPListBox";
+            this.IPListBox.Size = new System.Drawing.Size(485, 316);
+            this.IPListBox.TabIndex = 0;
+            // 
+            // IPTextBox
+            // 
+            this.IPTextBox.Location = new System.Drawing.Point(6, 376);
+            this.IPTextBox.Name = "IPTextBox";
+            this.IPTextBox.Size = new System.Drawing.Size(410, 31);
+            this.IPTextBox.TabIndex = 1;
+            // 
+            // PrefixComboBox
+            // 
+            this.PrefixComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PrefixComboBox.FormattingEnabled = true;
+            this.PrefixComboBox.IntegralHeight = false;
+            this.PrefixComboBox.Location = new System.Drawing.Point(422, 376);
+            this.PrefixComboBox.MaxDropDownItems = 4;
+            this.PrefixComboBox.Name = "PrefixComboBox";
+            this.PrefixComboBox.Size = new System.Drawing.Size(69, 32);
+            this.PrefixComboBox.TabIndex = 2;
+            // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(198, 266);
+            this.AddButton.AutoSize = true;
+            this.AddButton.Location = new System.Drawing.Point(325, 413);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(53, 23);
+            this.AddButton.Size = new System.Drawing.Size(80, 40);
             this.AddButton.TabIndex = 4;
             this.AddButton.Text = "Add";
             this.AddButton.UseVisualStyleBackColor = true;
@@ -65,46 +92,21 @@
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(257, 266);
+            this.DeleteButton.AutoSize = true;
+            this.DeleteButton.Location = new System.Drawing.Point(411, 414);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(53, 23);
+            this.DeleteButton.Size = new System.Drawing.Size(80, 40);
             this.DeleteButton.TabIndex = 3;
             this.DeleteButton.Text = "Delete";
             this.DeleteButton.UseVisualStyleBackColor = true;
             this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
-            // PrefixComboBox
-            // 
-            this.PrefixComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.PrefixComboBox.FormattingEnabled = true;
-            this.PrefixComboBox.IntegralHeight = false;
-            this.PrefixComboBox.Location = new System.Drawing.Point(271, 235);
-            this.PrefixComboBox.MaxDropDownItems = 4;
-            this.PrefixComboBox.Name = "PrefixComboBox";
-            this.PrefixComboBox.Size = new System.Drawing.Size(39, 25);
-            this.PrefixComboBox.TabIndex = 2;
-            // 
-            // IPTextBox
-            // 
-            this.IPTextBox.Location = new System.Drawing.Point(6, 236);
-            this.IPTextBox.Name = "IPTextBox";
-            this.IPTextBox.Size = new System.Drawing.Size(259, 23);
-            this.IPTextBox.TabIndex = 1;
-            // 
-            // IPListBox
-            // 
-            this.IPListBox.FormattingEnabled = true;
-            this.IPListBox.ItemHeight = 17;
-            this.IPListBox.Location = new System.Drawing.Point(6, 22);
-            this.IPListBox.Name = "IPListBox";
-            this.IPListBox.Size = new System.Drawing.Size(304, 208);
-            this.IPListBox.TabIndex = 0;
-            // 
             // ControlButton
             // 
-            this.ControlButton.Location = new System.Drawing.Point(253, 313);
+            this.ControlButton.AutoSize = true;
+            this.ControlButton.Location = new System.Drawing.Point(429, 487);
             this.ControlButton.Name = "ControlButton";
-            this.ControlButton.Size = new System.Drawing.Size(75, 23);
+            this.ControlButton.Size = new System.Drawing.Size(80, 40);
             this.ControlButton.TabIndex = 1;
             this.ControlButton.Text = "Save";
             this.ControlButton.UseVisualStyleBackColor = true;
@@ -112,12 +114,13 @@
             // 
             // GlobalBypassIPForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(340, 348);
-            this.Controls.Add(this.ControlButton);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(521, 539);
             this.Controls.Add(this.IPGroupBox);
-            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Controls.Add(this.ControlButton);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
@@ -128,6 +131,7 @@
             this.IPGroupBox.ResumeLayout(false);
             this.IPGroupBox.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
